c367d9b6964dcc8bc417619b92f901dea23bf409
j4nk0 <jan.gajdica@gmail.com>
2020-06-26 18:08:55 +0200

    add alert boxes

--- xbtzmenarendjango/xbtzmenarenapp/views.py
+++ xbtzmenarendjango/xbtzmenarenapp/views.py
@@ -5,10 +5,10 @@ from django.template import loader
 
 def buy(request):
     template = loader.get_template('xbtzmenarenapp/buy.html')
-    return HttpResponse(template.render({}, request))
+    return HttpResponse(template.render({'ok_message': "Nakup uspesny", 'error_message': "Nesapravna hodnota"}, request))
 
 def buy_action(request):
-    coin = request.POST['coin_sel']
-    sum_eur = request.POST['sum']
+    coin = request.POST['coin_selection']
+    sum_eur = request.POST['sum_eur']
     print(coin, sum_eur)
-    return HttpResponse("coin: " + coin + "sum: " + sum_eur)
+    return HttpResponse("coin: " + coin + "\n"  + "sum: " + sum_eur)
