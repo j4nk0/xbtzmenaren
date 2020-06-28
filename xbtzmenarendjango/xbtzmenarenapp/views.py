39d98a11cb21692a490899c45fc6fe9478263d09
j4nk0 <jan.gajdica@gmail.com>
2020-06-28 18:08:29 +0200

    make messages responsive

--- xbtzmenarendjango/xbtzmenarenapp/views.py
+++ xbtzmenarendjango/xbtzmenarenapp/views.py
@@ -3,32 +3,37 @@ from django.http import HttpResponse
 from django.template import loader
 
 
-def buy(request):
-    template = loader.get_template('xbtzmenarenapp/buy.html')
+def buy(request, success=None):
     context = {
-        'ok_message': "Nakup uspesny",
-        'error_message': "Nesapravna hodnota",
         'max_sum_eur': 55,
     }
-    return HttpResponse(template.render(context, request))
+    if success == True:
+        context.update({'ok_message': "Nákup uspešný"})
+    if success == False:
+        context.update({'error_message': "Nesprávna hodnota"})
+    #template = loader.get_template('xbtzmenarenapp/buy.html')
+    #return HttpResponse(template.render(context, request))
+    return render(request, 'xbtzmenarenapp/buy.html', context)
 
 def buy_btc(request):
     sum_eur = request.POST['sum_eur']
-    return HttpResponse("coin: Bitcoin" + "\n"  + "sum: " + sum_eur)
+    #return HttpResponse("coin: Bitcoin" + "\n"  + "sum: " + sum_eur)
+    return buy(request, True)
 
 def buy_ltc(request):
     sum_eur = request.POST['sum_eur']
     return HttpResponse("coin: Litecoin" + "\n"  + "sum: " + sum_eur)
 
-def sell(request):
-    template = loader.get_template('xbtzmenarenapp/sell.html')
+def sell(request, success=None):
     context = {
-        'ok_message': "Predaj uspesny",
-        'error_message': "Nesapravna hodnota",
         'max_sum_btc': 0.01,
         'max_sum_ltc': 2,
     }
-    return HttpResponse(template.render(context, request))
+    if success == True:
+        context.update({'ok_message': "Predaj uspešný"})
+    if success == False:
+        context.update({'error_message': "Nesprávna hodnota"})
+    return render(request, 'xbtzmenarenapp/sell.html', context)
 
 def sell_btc(request):
     sum_btc = request.POST['sum_btc']
