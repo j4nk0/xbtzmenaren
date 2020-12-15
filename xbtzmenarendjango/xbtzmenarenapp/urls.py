467dc41dbc19c218fc2d925bfee9127b825a4c0b
j4nk0 <jan.gajdica@gmail.com>
2020-12-15 14:51:04 +0100

    finish adding xlm and zrx

--- xbtzmenarendjango/xbtzmenarenapp/urls.py
+++ xbtzmenarendjango/xbtzmenarenapp/urls.py
@@ -12,12 +12,16 @@ urlpatterns = [
     path('buy-ltc/', views.buy_ltc, name='buy-ltc'),
     path('buy-doge/', views.buy_doge, name='buy-doge'),
     path('buy-eth/', views.buy_eth, name='buy-eth'),
+    path('buy-xlm/', views.buy_xlm, name='buy-xlm'),
+    path('buy-zrx/', views.buy_zrx, name='buy-zrx'),
 
     path('sell/', views.sell, name='sell'),
     path('sell-btc/', views.sell_btc, name='sell-btc'),
     path('sell-ltc/', views.sell_ltc, name='sell-ltc'),
     path('sell-doge/', views.sell_doge, name='sell-doge'),
     path('sell-eth/', views.sell_eth, name='sell-eth'),
+    path('sell-xlm/', views.sell_xlm, name='sell-xlm'),
+    path('sell-zrx/', views.sell_zrx, name='sell-zrx'),
 
     path('limit-order-buy/', views.limit_order_buy, name='limit-order-buy'),
     path('limit-order-sell/', views.limit_order_sell, name='limit-order-sell'),
@@ -29,6 +33,10 @@ urlpatterns = [
     path('limit-order-buy-doge/json', views.limit_order_buy_doge_json, name='limit-order-buy-doge-json'),
     path('limit-order-buy-eth/', views.limit_order_buy_eth, name='limit-order-buy-eth'),
     path('limit-order-buy-eth/json', views.limit_order_buy_eth_json, name='limit-order-buy-eth-json'),
+    path('limit-order-buy-xlm/', views.limit_order_buy_xlm, name='limit-order-buy-xlm'),
+    path('limit-order-buy-xlm/json', views.limit_order_buy_xlm, name='limit-order-buy-xlm-json'),
+    path('limit-order-buy-zrx/', views.limit_order_buy_zrx, name='limit-order-buy-zrx'),
+    path('limit-order-buy-zrx/json', views.limit_order_buy_zrx_json, name='limit-order-buy-zrx-json'),
 
     path('limit-order-sell-btc/', views.limit_order_sell_btc, name='limit-order-sell-btc'),
     path('limit-order-sell-btc/json', views.limit_order_sell_btc_json, name='limit-order-sell-btc-json'),
@@ -38,16 +46,24 @@ urlpatterns = [
     path('limit-order-sell-doge/json', views.limit_order_sell_doge_json, name='limit-order-sell-doge-json'),
     path('limit-order-sell-eth/', views.limit_order_sell_eth, name='limit-order-sell-eth'),
     path('limit-order-sell-eth/json', views.limit_order_sell_eth_json, name='limit-order-sell-eth-json'),
+    path('limit-order-sell-xlm/', views.limit_order_sell_xlm, name='limit-order-sell-xlm'),
+    path('limit-order-sell-xlm/json', views.limit_order_sell_xlm_json, name='limit-order-sell-xlm-json'),
+    path('limit-order-sell-zrx/', views.limit_order_sell_zrx, name='limit-order-sell-zrx'),
+    path('limit-order-sell-zrx/json', views.limit_order_sell_zrx_json, name='limit-order-sell-zrx-json'),
 
     path('limit-order-buy-btc-delete/<int:order_id>', views.limit_order_buy_btc_delete, name='limit-order-buy-btc-delete'),
     path('limit-order-buy-ltc-delete/<int:order_id>', views.limit_order_buy_ltc_delete, name='limit-order-buy-ltc-delete'),
     path('limit-order-buy-doge-delete/<int:order_id>', views.limit_order_buy_doge_delete, name='limit-order-buy-doge-delete'),
     path('limit-order-buy-eth-delete/<int:order_id>', views.limit_order_buy_eth_delete, name='limit-order-buy-eth-delete'),
+    path('limit-order-buy-xlm-delete/<int:order_id>', views.limit_order_buy_xlm_delete, name='limit-order-buy-xlm-delete'),
+    path('limit-order-buy-zrx-delete/<int:order_id>', views.limit_order_buy_zrx_delete, name='limit-order-buy-zrx-delete'),
 
     path('limit-order-sell-btc-delete/<int:order_id>', views.limit_order_sell_btc_delete, name='limit-order-sell-btc-delete'),
     path('limit-order-sell-ltc-delete/<int:order_id>', views.limit_order_sell_ltc_delete, name='limit-order-sell-ltc-delete'),
     path('limit-order-sell-doge-delete/<int:order_id>', views.limit_order_sell_doge_delete, name='limit-order-sell-doge-delete'),
     path('limit-order-sell-eth-delete/<int:order_id>', views.limit_order_sell_eth_delete, name='limit-order-sell-eth-delete'),
+    path('limit-order-sell-xlm-delete/<int:order_id>', views.limit_order_sell_xlm_delete, name='limit-order-sell-xlm-delete'),
+    path('limit-order-sell-zrx-delete/<int:order_id>', views.limit_order_sell_zrx_delete, name='limit-order-sell-zrx-delete'),
 
     path('rates/', views.private_rates, name='rates'),
     path('rates/json', views.rates_json, name='rates-json'),
@@ -55,11 +71,15 @@ urlpatterns = [
     path('buy-ltc/json', views.buy_ltc_json, name='buy-ltc-json'),
     path('buy-doge/json', views.buy_doge_json, name='buy-doge-json'),
     path('buy-eth/json', views.buy_eth_json, name='buy-eth-json'),
+    path('buy-xlm/json', views.buy_xlm_json, name='buy-xlm-json'),
+    path('buy-zrx/json', views.buy_zrx_json, name='buy-zrx-json'),
 
     path('sell-btc/json', views.sell_btc_json, name='sell-btc-json'),
     path('sell-ltc/json', views.sell_ltc_json, name='sell-ltc-json'),
     path('sell-doge/json', views.sell_doge_json, name='sell-doge-json'),
     path('sell-eth/json', views.sell_eth_json, name='sell-eth-json'),
+    path('sell-xlm/json', views.sell_xlm_json, name='sell-xlm-json'),
+    path('sell-zrx/json', views.sell_zrx_json, name='sell-zrx-json'),
 
     path('terms/', views.terms_and_conditions, name='terms'),
 
@@ -81,6 +101,8 @@ urlpatterns = [
     path('withdrawal-ltc/', views.withdrawal_ltc, name='withdrawal-ltc'),
     path('withdrawal-doge/', views.withdrawal_doge, name='withdrawal-doge'),
     path('withdrawal-eth/', views.withdrawal_eth, name='withdrawal-eth'),
+    path('withdrawal-xlm/', views.withdrawal_xlm, name='withdrawal-xlm'),
+    path('withdrawal-zrx/', views.withdrawal_zrx, name='withdrawal-zrx'),
 
     path('management/verification/', views.management_verification, name='management-verification'),
     path('management/verification-attempt/', views.management_verification_attempt, name='management-verification-attempt'),
@@ -91,6 +113,8 @@ urlpatterns = [
     path('management/withdrawal-ltc-check/<int:withdrawal_id>', views.management_withdrawal_ltc_check, name='management-withdrawal-ltc-check'),
     path('management/withdrawal-doge-check/<int:withdrawal_id>', views.management_withdrawal_doge_check, name='management-withdrawal-doge-check'),
     path('management/withdrawal-eth-check/<int:withdrawal_id>', views.management_withdrawal_eth_check, name='management-withdrawal-eth-check'),
+    path('management/withdrawal-xlm-check/<int:withdrawal_id>', views.management_withdrawal_xlm_check, name='management-withdrawal-xlm-check'),
+    path('management/withdrawal-zrx-check/<int:withdrawal_id>', views.management_withdrawal_zrx_check, name='management-withdrawal-zrx-check'),
 
     path('management/deposits/', views.management_deposits, name='management-deposits'),
     path('management/deposit-attempt/', views.management_deposit_attempt, name='deposit-eur-attempt'),
