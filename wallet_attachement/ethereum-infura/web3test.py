0fa155c939c1e6e4bf537488ee95bde13fef47af
j4nk0 <jan.gajdica@gmail.com>
2020-12-17 20:37:15 +0100

    delete ethereum driver attempt

--- wallet_attachement/ethereum-infura/web3test.py
+++
@@ -1,7 +0,0 @@
-from web3 import Web3
-
-provider = Web3.WebsocketProvider('wss://ropsten.infura.io/ws/v3/202896bbe2ad4e428a1fbe4b14a93e99')
-
-w3 = Web3(provider)
-print(w3.isConnected())
-print(w3.eth.getBlock('latest'))
