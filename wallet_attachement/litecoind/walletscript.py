241d15d55d24bb1119b913cf093afe64f7984228
j4nk0 <jan.gajdica@gmail.com>
2021-02-11 15:04:57 +0100

    fix litecoin scripts

--- wallet_attachement/litecoind/walletscript.py
+++ wallet_attachement/litecoind/walletscript.py
@@ -1,6 +1,12 @@
 import sys
 import socket
 
-socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-socket.connect(('localhost', 9331))
-socket.send(('NEWTX:' + sys.argv[1] + '*').encode(encoding='UTF-8'))
+my_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+while True:
+    try:
+        my_socket.connect(('localhost', 9331))
+        break
+    except socket.error:
+        pass
+my_socket.sendall(('NEWTX:' + sys.argv[1] + '*').encode(encoding='UTF-8'))
+my_socket.close()
