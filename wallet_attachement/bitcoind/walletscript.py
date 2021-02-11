24afb58faa441bddaf8f4511cf5bcda9ad5367a2
j4nk0 <jan.gajdica@gmail.com>
2021-02-11 15:15:52 +0100

    fix bitcoin and dogecoin scripts

--- wallet_attachement/bitcoind/walletscript.py
+++ wallet_attachement/bitcoind/walletscript.py
@@ -1,6 +1,12 @@
 import sys
 import socket
 
-socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-socket.connect(('localhost', 8331))
-socket.send(('NEWTX:' + sys.argv[1] + '*').encode(encoding='UTF-8'))
+my_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+while True:
+    try:
+        my_socket.connect(('localhost', 8331))
+        break
+    except:
+        pass
+my_socket.sendall(('NEWTX:' + sys.argv[1] + '*').encode(encoding='UTF-8'))
+my_socket.close()
