e2584cfcdd8e5ee1c64fa4dd6f8c72bf0f3bcb8c
j4nk0 <jan.gajdica@gmail.com>
2021-02-12 14:26:15 +0100

    switch litecoin_driver to pipes

--- wallet_attachement/litecoind/blockscript.py
+++ wallet_attachement/litecoind/blockscript.py
@@ -1,12 +1,5 @@
 import sys
-import socket
 
-my_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-while True:
-    try:
-        my_socket.connect(('localhost', 9331))
-        break
-    except socket.error:
-        pass
-my_socket.sendall(('NEWBLOCK:' + sys.argv[1] + '*').encode(encoding='UTF-8'))
-my_socket.close()
+with open('/run/litecoin_block', 'w') as f:
+    f.write(sys.argv[1] + '*')
+
