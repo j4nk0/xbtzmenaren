b2743a2dc204cb351097c55ef6a8d0844165321a
j4nk0 <jan.gajdica@gmail.com>
2021-02-19 09:10:37 +0100

    wallet attachement configs

--- wallet_attachement/litecoind/blockscript.py
+++
@@ -1,5 +0,0 @@
-import sys
-
-with open('/run/litecoin_block', 'w') as f:
-    f.write(sys.argv[1] + '*')
-
--- wallet_attachement/litecoind/walletscript.py
+++
@@ -1,5 +0,0 @@
-import sys
-
-with open('/run/litecoin_tx', 'w') as f:
-    f.write(sys.argv[1] + '*')
-
