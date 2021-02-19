4874eab2af6f1aa6d134c86d6435c16fe61ffa9c
j4nk0 <jan.gajdica@gmail.com>
2021-02-19 09:24:37 +0100

    fix wsgi

--- xbtzmenarendjango/xbtzmenarendjango/wsgi.py
+++ xbtzmenarendjango/xbtzmenarendjango/wsgi.py
@@ -8,6 +8,9 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
 
 import os
+import sys
+
+sys.path.append('/var/www/xbtzmenarendjango')
 
 from django.core.wsgi import get_wsgi_application
 
